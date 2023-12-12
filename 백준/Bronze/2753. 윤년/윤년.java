b8cbf175d6fd621644bdf0057a2b87aec726d84a
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2023-12-12 17:23:23 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(isleapYear(year));
+    }
+
+    private static Integer isleapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            return 1;
+        }
+        return 0;
+    }
+}
