cc369e463b3f58323494811d7f041ff2c283bb99
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2023-12-13 18:01:54 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 216 ms, Memory: 17856 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printAnswer(n);
+    }
+
+    private static void printAnswer(int n) {
+        String star = "*";
+        String empty = " ";
+        for (int i = 1; i <= n; i++) {
+            System.out.print(empty.repeat(n - i));
+            System.out.println(star.repeat(i));
+        }
+    }
+}
+
