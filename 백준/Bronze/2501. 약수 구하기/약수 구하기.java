e003876743ad9cff93635fb541980b74a900e57e
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-01-02 22:33:20 +0900

    [Bronze III] Title: 약수 구하기, Time: 212 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+        printAnswer(list, k);
+    }
+
+    private static void printAnswer(List<Integer> list, int k) {
+        if (list.isEmpty() || k > list.size()) {
+            System.out.println(0);
+            return;
+        }
+
+        System.out.println(list.get(k - 1));
+    }
+}
