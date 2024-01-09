afa0c861ed31431486a40b0b5839ddae430520ce
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-01-09 15:27:08 +0900

    [Silver V] Title: 최소공배수, Time: 220 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println(getAnswer(a, b));
+
+    }
+
+    private static long getAnswer(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
