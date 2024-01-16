fea0d40e685f6beadabc9ce2adbf90426e762ea2
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-01-16 19:44:18 +0900

    [Silver III] Title: N과 M (2), Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] tmp;
+    static boolean[] check;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        tmp = new int[n + 1];
+        check = new boolean[n + 1];
+
+        rec(1);
+
+        System.out.print(sb);
+    }
+
+    private static void rec(int pos) {
+        if (pos == m + 1) {
+            for (int i = 1; i <= m; i++) {
+                sb.append(tmp[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (check[i] == false && tmp[pos - 1] < i) {
+                check[i] = true;
+                tmp[pos] = i;
+                rec(pos + 1);
+                check[i] = false;
+            }
+        }
+    }
+}
