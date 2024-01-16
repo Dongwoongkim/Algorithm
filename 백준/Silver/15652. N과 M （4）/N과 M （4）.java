183f2c679df4b5987d8b0bf786010b6122d157a6
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-01-16 19:53:09 +0900

    [Silver III] Title: N과 M (4), Time: 244 ms, Memory: 19856 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] tmp;
+    static boolean[] check;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        tmp = new int[n + 1];
+        check = new boolean[n + 1];
+
+        rec(0, 1);
+
+        System.out.print(sb);
+    }
+
+    private static void rec(int pos, int next) {
+        if (pos == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(tmp[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = next; i <= n; i++) {
+            tmp[pos] = i;
+            rec(pos + 1, tmp[pos]);
+        }
+    }
+}
