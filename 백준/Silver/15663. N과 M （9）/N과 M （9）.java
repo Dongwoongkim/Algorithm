b2a341c74abeffdd96fc6e969de5040abef67dad
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-01-18 22:45:50 +0900

    [Silver II] Title: N과 M (9), Time: 588 ms, Memory: 81464 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,62 @@
+import static java.util.Arrays.sort;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] tmp;
+    static int[] arr;
+    static boolean[] check;
+    static LinkedHashSet<String> set;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        tmp = new int[n + 1];
+        arr = new int[n];
+        check = new boolean[n + 1];
+        set = new LinkedHashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sort(arr);
+        rec(0);
+
+        for (String line : set) {
+            sb.append(line);
+        }
+
+        System.out.print(sb);
+    }
+
+    private static void rec(int pos) {
+        if (pos == m) {
+            String ans = "";
+            for (int i = 0; i < m; i++) {
+                ans += String.valueOf(tmp[i]);
+                ans += " ";
+            }
+            ans += "\n";
+            set.add(ans);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (check[i] == false) {
+                check[i] = true;
+                tmp[pos] = arr[i];
+                rec(pos + 1);
+                check[i] = false;
+            }
+        }
+    }
+}
