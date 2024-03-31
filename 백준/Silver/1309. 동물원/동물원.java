16fd25e073165fba992d8956c2156f834c026137
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-03-31 19:13:32 +0900

    [Silver I] Title: 동물원, Time: 220 ms, Memory: 21200 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int[][] dp = new int[100001][3];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long[] dp = new long[n + 2];
-        dp[1] = 3;
-        dp[2] = 7;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = ((dp[i - 1] * 2) + dp[i - 2]) % 9901;
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][0]) % 9901;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
         }
 
-        System.out.println(dp[n] % 9901);
+        System.out.println((dp[n][0] + dp[n][1] + dp[n][2]) % 9901);
     }
 }
