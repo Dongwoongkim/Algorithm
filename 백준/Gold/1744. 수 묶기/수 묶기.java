de6e7d51307818639dd966fc1fce9497364a9755
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-04-10 17:38:35 +0900

    [Gold IV] Title: 수 묶기, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -11,15 +11,14 @@ public class Main {
     static List<Integer> positive = new ArrayList<>();
     static int zeroCnt = 0;
     static int oneCnt = 0;
-
-    static int ans = 0;
+    static Long ans = 0L;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-
         arr = new int[n + 1];
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
@@ -47,36 +46,33 @@ public class Main {
 
         Collections.reverse(positive);
 
-        if (negative.size() % 2 == 0) {
-            for (int i = 0; i < negative.size(); i += 2) {
-                ans += negative.get(i) * negative.get(i + 1);
-            }
-        } else if (negative.size() % 2 == 1) {
-            for (int i = 0; i < negative.size() - 1; i += 2) {
-                ans += negative.get(i) * negative.get(i + 1);
-            }
+        for (int i = 0; i < negative.size() - 1; i += 2) {
+            ans += negative.get(i) * negative.get(i + 1);
+        }
 
-            if (zeroCnt == 0) {
+        if (negative.size() % 2 == 1) {
+            // 0이 없고 1만 있는 경우
+            if (zeroCnt == 0 && oneCnt >= 1) {
                 oneCnt--;
                 ans += negative.get(negative.size() - 1) + 1;
+                // 0도 없고 1도 없는 경우
+            } else if (zeroCnt == 0 && oneCnt == 0) {
+                ans += negative.get(negative.size() - 1);
             }
         }
 
-        ans += (oneCnt) * 1;
+        if (oneCnt >= 1) {
+            ans += (oneCnt) * 1;
+        }
 
-        if (positive.size() % 2 == 0) {
-            for (int i = 0; i < positive.size(); i += 2) {
-                ans += positive.get(i) * positive.get(i + 1);
-            }
-        } else if (positive.size() % 2 == 1) {
-            for (int i = 0; i < positive.size() - 1; i += 2) {
-                ans += positive.get(i) * positive.get(i + 1);
-            }
+        for (int i = 0; i < positive.size() - 1; i += 2) {
+            ans += positive.get(i) * positive.get(i + 1);
+        }
 
+        if (positive.size() % 2 == 1) {
             ans += positive.get(positive.size() - 1);
         }
 
         System.out.println(ans);
-
     }
 }
