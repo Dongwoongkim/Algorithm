b0568b128b5bb0126a393bc0861e555d20a3364a
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-04-18 14:33:32 +0900

    [Silver V] Title: 폴리오미노, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -8,35 +8,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
 
-        int idx = 0;
+        String s = line.replaceAll("XXXX", "AAAA");
+        String ans = s.replaceAll("XX", "BB");
 
-        String[] split = line.split("[.]");
-        for (String s : split) {
-            if (s.length() % 2 == 1) {
-                System.out.println(-1);
-                return;
-            }
-        }
-
-        while (true) {
-            if (idx >= line.length()) {
-                break;
-            }
-
-            if (idx + 4 <= line.length() && line.substring(idx, idx + 4).equals("XXXX")) {
-                System.out.print("AAAA");
-                idx += 4;
-                continue;
-            }
-
-            if (idx + 2 <= line.length() && line.substring(idx, idx + 2).equals("XX")) {
-                System.out.print("BB");
-                idx += 2;
-                continue;
-            }
-
-            System.out.print(line.charAt(idx));
-            idx++;
+        if (ans.contains("X")) {
+            System.out.println(-1);
+        } else {
+            System.out.println(ans);
         }
     }
 }
