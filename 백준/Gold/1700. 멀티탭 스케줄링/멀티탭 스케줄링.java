8e51388bd90a88f1f49f34cd5613aee863923e40
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-05-15 15:48:35 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 216 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -6,52 +6,56 @@ public class Main {
 
     static int n;
     static int k;
-    static int use = 0;
-    static int cnt = 0;
-    static List<Integer> p = new ArrayList<>();
-    static List<Integer> outlet = new ArrayList<>();
+    static int[] frequency;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         k = sc.nextInt();
+        int cnt = 0;
+
+        List<Integer> pN = new ArrayList<>();
+        List<Integer> plug = new ArrayList<>();
+        frequency = new int[n + 1];
 
         for (int i = 0; i < k; i++) {
             int num = sc.nextInt();
-            p.add(num);
+            pN.add(num);
         }
 
         for (int i = 0; i < k; i++) {
-            if (outlet.contains(p.get(i))) {
+            // 꼽혀있는 경우
+            if (plug.contains(pN.get(i))) {
                 continue;
             }
 
-            if (use < n) {
-                use++;
-                outlet.add(p.get(i));
+            // 꼽혀있지 않고 콘센트에 자리가 있는 경우
+            if (plug.size() < n) {
+                plug.add(pN.get(i));
                 continue;
             }
 
-            if (use == n) {
-                int pos = -1; // 멀티탭에서 뺄 자리
-                int idx = -1; // 스케줄에서 가장 마지막에 잡혀있는 것
-                for (int l = 0; l < n; l++) {
-                    int tmp = 0;
-                    for (int j = i + 1; j < k; j++) {
-                        if (outlet.get(l) == p.get(j)) {
-                            break;
-                        }
-                        tmp++;
+            // 꼽혀있지 않고 콘센트에 자리가 없는 경우
+
+            int removeIdx = -1;
+            int pos = -1;
+            for (int j = 0; j < n; j++) {
+                int tmp = 0;
+                for (int l = i + 1; l < k; l++) {
+                    if (plug.get(j) == pN.get(l)) {
+                        break;
                     }
-                    if (tmp > idx) {
-                        pos = l;
-                        idx = tmp;
-                    }
+                    tmp++;
                 }
 
-                outlet.set(pos, p.get(i));
-                cnt++;
+                if (tmp > removeIdx) {
+                    pos = j;
+                    removeIdx = tmp;
+                }
             }
+
+            plug.set(pos, pN.get(i));
+            cnt++;
         }
 
         System.out.println(cnt);
