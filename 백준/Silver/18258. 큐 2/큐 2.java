d540dfb44ce8eb790be147d88598f668afb8830f
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-05-16 14:21:53 +0900

    [Silver IV] Title: 큐 2, Time: 1556 ms, Memory: 407900 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,64 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
-import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
 
-        Queue<Integer> q = new LinkedList<>();
-
-        int back = 0;
+        int back = -1;
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
-            if (line.contains("push")) {
-                List<String> command = Arrays.stream(line.split(" ")).collect(Collectors.toList());
-                int number = Integer.parseInt(command.get(1));
+            String[] commands = line.split(" ");
+
+            if (commands[0].equals("push")) {
+                int number = Integer.parseInt(commands[1]);
+                queue.add(number);
                 back = number;
-                q.add(number);
             }
 
             if (line.equals("pop")) {
-                if (q.isEmpty()) {
-                    sb.append(-1).append("\n");
-                    continue;
+                if (queue.isEmpty()) {
+                    sb.append("-1").append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
                 }
-                sb.append(q.remove()).append("\n");
-            }
-
-            if (line.equals("front")) {
-                if (q.isEmpty()) {
-                    sb.append(-1).append("\n");
-                    continue;
-                }
-                sb.append(q.peek()).append("\n");
-            }
-
-            if (line.equals("back")) {
-                if (q.isEmpty()) {
-                    sb.append(-1).append("\n");
-                    continue;
-                }
-                sb.append(back).append("\n");
-            }
-
-            if (line.equals("empty")) {
-                if (q.isEmpty()) {
-                    sb.append(1).append("\n");
-                    continue;
-                }
-                sb.append(0).append("\n");
             }
 
             if (line.equals("size")) {
-                sb.append(q.size()).append("\n");
+                sb.append(queue.size()).append("\n");
+            }
+
+            if (line.equals("empty")) {
+                if (queue.isEmpty()) {
+                    sb.append("1").append("\n");
+                } else {
+                    sb.append("0").append("\n");
+                }
+            }
+
+            if (line.equals("front")) {
+                if (queue.isEmpty()) {
+                    sb.append("-1").append("\n");
+                } else {
+                    sb.append(queue.peek()).append("\n");
+                }
+            }
+
+            if (line.equals("back")) {
+                if (queue.isEmpty()) {
+                    sb.append("-1").append("\n");
+                } else {
+                    sb.append(back).append("\n");
+                }
             }
         }
 
