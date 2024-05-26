cedfacd8c67354ecc5326623a9271e54734d98f1
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-05-26 19:54:01 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,34 +1,37 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
 
-        if (n == 0) {
-            System.out.println(0);
-            return;
+        // n!의 2의 지수
+        // n을 소인수분해했을때 2의 지수
+        // 지수 곱은 합으로 계산할수 있으므로
+        int twoCnt = get(n, 2);
+
+        // n!에서 5의 지수
+        // n을 소인수분해했을때 5의 지수
+        int fiveCnt = get(n, 5);
+
+        // n! = 2^(twoCnt) * 5^(fiveCnt) * ...
+        // n! = (10)^(Math.min(twoCnt, fiveCnt)) * ...
+        System.out.println(Math.min(twoCnt, fiveCnt));
+    }
+
+    private static int get(int n, int k) {
+        int cnt = 0;
+
+        while (n >= k) {
+            cnt += n / k;
+            n /= k;
         }
 
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % 5 == 0) {
-                ans++;
-            }
-            if (i % 25 == 0) {
-                ans++;
-            }
-            if (i % 125 == 0) {
-                ans++;
-            }
-            if (i % 625 == 0) {
-                ans++;
-            }
-        }
-
-        System.out.println(ans);
+        return cnt;
     }
 }
