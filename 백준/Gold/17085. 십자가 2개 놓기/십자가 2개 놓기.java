1479268039d4e545abd461b4533b026a01532260
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-06-03 15:20:14 +0900

    [Gold IV] Title: 십자가 2개 놓기, Time: 140 ms, Memory: 16596 KB -BaekjoonHub

--- 백준/Gold/17085. 십자가 2개 놓기/십자가 2개 놓기.java
+++ 백준/Gold/17085. 십자가 2개 놓기/십자가 2개 놓기.java
@@ -1,55 +1,67 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    static int n;
+    static int m;
     static int ans = -1;
     static char[][] map;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        char[][] area = new char[n][m];
+        map = new char[n + 1][m + 1];
 
         for (int i = 0; i < n; i++) {
-            String line = scanner.next();
+            String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                area[i][j] = line.charAt(j);
+                map[i][j] = line.charAt(j);
             }
         }
 
-        int ans = 0;
+        System.out.println(solution());
+    }
 
-        for (int y1 = 0; y1 < n; y1++) {
-            for (int x1 = 0; x1 < m; x1++) {
+    private static int solution() {
+        int ans = -1;
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < m; y++) {
                 // 십자가 1
-                for (int l = 0; ; l++) {
-                    // 범위밖
-                    if (x1 - l < 0 || x1 + l >= m || y1 - l < 0 || y1 + l >= n) {
+                for (int size = 0; ; size++) {
+                    if (x - size < 0 || y - size < 0 || x + size >= n || y + size >= m) {
                         break;
                     }
-                    if (area[y1][x1 + l] != '#' || area[y1 + l][x1] != '#' || area[y1][x1 - l] != '#'
-                            || area[y1 - l][x1] != '#') {
+
+                    if (map[x - size][y] != '#' || map[x + size][y] != '#' ||
+                            map[x][y - size] != '#' || map[x][y + size] != '#') {
                         break;
                     }
-                    area[y1][x1 + l] = '*';
-                    area[y1][x1 - l] = '*';
-                    area[y1 + l][x1] = '*';
-                    area[y1 - l][x1] = '*';
+
+                    map[x + size][y] = '*';
+                    map[x - size][y] = '*';
+                    map[x][y + size] = '*';
+                    map[x][y - size] = '*';
 
                     // 십자가 2
-                    for (int y2 = 0; y2 < n; y2++) {
-                        for (int x2 = 0; x2 < m; x2++) {
-                            for (int l2 = 0; ; l2++) {
-                                if (x2 - l2 < 0 || x2 + l2 >= m || y2 - l2 < 0 || y2 + l2 >= n) {
+                    for (int a = 0; a < n; a++) {
+                        for (int b = 0; b < m; b++) {
+                            for (int size2 = 0; ; size2++) {
+                                if (a - size2 < 0 || b - size2 < 0 || a + size2 >= n || b + size2 >= m) {
                                     break;
                                 }
-                                if (area[y2][x2 + l2] != '#' || area[y2][x2 - l2] != '#' || area[y2 + l2][x2] != '#'
-                                        || area[y2 - l2][x2] != '#') {
+                                if (map[a - size2][b] != '#' || map[a + size2][b] != '#' ||
+                                        map[a][b - size2] != '#' || map[a][b + size2] != '#') {
                                     break;
                                 }
-                                ans = Math.max((4 * l + 1) * (4 * l2 + 1), ans);
+
+                                int area = ((size) * 4 + 1) * ((size2) * 4 + 1);
+                                ans = Math.max(ans, area);
                             }
                         }
                     }
@@ -57,22 +69,22 @@ public class Main {
 
                 // 십자가 1 원상복구
                 for (int l = 0; ; l++) {
-                    if (x1 - l < 0 || x1 + l >= m || y1 - l < 0 || y1 + l >= n) {
+                    if (x - l < 0 || y - l < 0 || x + l >= n || y + l >= m) {
                         break;
                     }
-                    if (area[y1][x1 + l] != '*' || area[y1 + l][x1] != '*' || area[y1][x1 - l] != '*'
-                            || area[y1 - l][x1] != '*') {
+                    if (map[x - l][y] != '*' || map[x + l][y] != '*' ||
+                            map[x][y - l] != '*' || map[x][y + l] != '*') {
                         break;
                     }
 
-                    area[y1][x1 + l] = '#';
-                    area[y1][x1 - l] = '#';
-                    area[y1 + l][x1] = '#';
-                    area[y1 - l][x1] = '#';
+                    map[x][y + l] = '#';
+                    map[x][y - l] = '#';
+                    map[x + l][y] = '#';
+                    map[x - l][y] = '#';
                 }
             }
         }
 
-        System.out.println(ans);
+        return ans;
     }
 }
