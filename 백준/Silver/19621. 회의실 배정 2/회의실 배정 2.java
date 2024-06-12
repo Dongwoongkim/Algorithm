60cb5cb571c4d440c110bd2dd0f657b8f0e329de
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2024-06-13 00:30:20 +0900

    [Silver II] Title: 회의실 배정 2, Time: 128 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.java
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.java
@@ -6,8 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int n;
-    static int ans = Integer.MIN_VALUE;
-    static boolean[] check;
+    static int[] dp;
     static int[] arr;
 
     public static void main(String[] args) throws IOException {
@@ -15,28 +14,24 @@ public class Main {
         n = Integer.parseInt(br.readLine());
 
         arr = new int[n + 1];
+        dp = new int[n + 1];
+
         for (int i = 1; i <= n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-
             arr[i] = v;
         }
 
-        dfs(1, 0);
+        dp[1] = arr[1];
 
-        System.out.println(ans);
-    }
-
-    private static void dfs(int next, int sum) {
-        if (next > n) {
-            ans = Math.max(ans, sum);
-            return;
+        for (int i = 2; i <= n; i++) {
+            // 전전꺼 선택하고 현재꺼 선택 vs 현재꺼 선택안하고 바로 이전꺼 선택한 선택
+            dp[i] = Math.max(dp[i - 2] + arr[i], dp[i - 1]);
         }
 
-        dfs(next + 1, sum);
-        dfs(next + 2, sum + arr[next]);
+        System.out.println(dp[n]);
     }
+
 }
