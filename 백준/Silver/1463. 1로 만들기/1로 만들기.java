0541fed051b9ab662f4837982acc3e754a904acb
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2025-11-11 21:14:56 +0900

    [Silver III] Title: 1로 만들기, Time: 188 ms, Memory: 21620 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 
 public class Main {
 
+    static int[] dp = new int[1_000_001];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
 
-        int[] dp = new int[n * 3 + 1];
-
         dp[1] = 0;
-        for (int i = 2; i <= n; i++) {
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
         }
 
-        for (int i = 1; i <= n; i++) {
-            dp[i * 2] = Math.min(dp[i] + 1, dp[i * 2]);
-            dp[i * 3] = Math.min(dp[i] + 1, dp[i * 3]);
-            dp[i + 1] = Math.min(dp[i] + 1, dp[i + 1]);
-        }
-
-        System.out.println(dp[n]);
+        System.out.print(dp[n]);
     }
 }
