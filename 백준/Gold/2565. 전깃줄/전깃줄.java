238f6ceb6cabaabffc27d953d758f319565d811d
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2025-11-20 21:44:28 +0900

    [Gold V] Title: 전깃줄, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -1,34 +1,32 @@
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    static int[] dp = new int[101];
 
-        int[] arr = new int[n + 1];
-        int[] dp = new int[n + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        Map<Integer, Integer> map = new TreeMap<>();
+        List<Pair> pairs = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            map.put(sc.nextInt(), sc.nextInt());
-        }
-
-        int idx = 0;
-        for (Integer value : map.values()) {
-            arr[idx++] = value;
-        }
-        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            pairs.add(new Pair(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
             dp[i] = 1;
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] > arr[i]) {
-                    dp[j] = Math.max(dp[j], dp[i] + 1);
+        pairs.sort((a, b) -> a.start - b.start);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs.get(j).end < pairs.get(i).end) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
@@ -38,5 +36,18 @@ public class Main {
             ans = Math.max(ans, dp[i]);
         }
         System.out.println(n - ans);
+
     }
+
+    static class Pair {
+
+        int start;
+        int end;
+
+        public Pair(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
 }
