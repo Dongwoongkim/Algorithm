fd93d9c36436e0955f164cc0417f84afae30f222
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2025-11-24 22:46:16 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,47 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int cnt = 0;
+    static int n, m;
     static List<List<Integer>> vector = new ArrayList<>();
     static boolean[] check;
+    static int ans = 0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        check = new boolean[n + 1];
 
         for (int i = 0; i <= n; i++) {
             vector.add(new ArrayList<>());
         }
-        check = new boolean[n + 1];
 
         for (int i = 0; i < m; i++) {
-            int from = sc.nextInt();
-            int to = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-            vector.get(from).add(to);
-            vector.get(to).add(from);
-        }
-
-        for (int i = 1; i <= n; i++) {
-            Collections.sort(vector.get(i));
+            vector.get(start).add(end);
+            vector.get(end).add(start);
         }
 
         dfs(1);
-        System.out.println(cnt);
+
+        System.out.print(ans);
     }
 
-    private static void dfs(int start) {
-        check[start] = true;
+    private static void dfs(int node) {
+        check[node] = true;
 
-        for (int i = 0; i < vector.get(start).size(); i++) {
-            int next = vector.get(start).get(i);
-            if (check[next] == false) {
-                cnt++;
+        if (node != 1) {
+            ans++;
+        }
+
+        for (int next : vector.get(node)) {
+            if (!check[next]) {
                 dfs(next);
             }
         }
