a03e5b37fa980e345d0c52032c1a89a7c4906f0b
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2025-12-01 22:24:38 +0900

    [Silver I] Title: 숨바꼭질, Time: 176 ms, Memory: 21100 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,63 +1,85 @@
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n;
-    static int k;
-    static List<List<Integer>> v = new ArrayList<>();
-    static boolean[] check = new boolean[100001];
-    static int[] ans = new int[100001];
+	static int n, m;
+	static int[] step = new int[100_001 * 2];
+	static boolean[] check = new boolean[100_001 * 2];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        k = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i <= 100000; i++) {
-            v.add(new ArrayList<>());
-        }
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-        bfs(n);
-        System.out.println(ans[k]);
-    }
+		for (int i = 1; i <= 200_000; i++) {
+			step[i] = Integer.MAX_VALUE;
+		}
 
-    private static void bfs(int n) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(n);
-        check[n] = true;
-        ans[n] = 0;
+		step[n] = 0;
 
-        while (!q.isEmpty()) {
-            Integer current = q.peek();
-            q.poll();
+		System.out.println(bfs(new Position(n, 0)));
+	}
 
-            int left = current - 1;
-            int middle = current + 1;
-            int right = current * 2;
+	private static int bfs(Position position) {
+		int answer = -1;
+		Queue<Position> queue = new LinkedList<>();
+		queue.add(position);
+		check[position.pos] = true;
 
-            if (left >= 0 && left <= 100000 && !check[left]) {
-                check[left] = true;
-                ans[left] = ans[current] + 1;
-                q.add(left);
-            }
-            if (middle >= 0 && middle <= 100000 && !check[middle]) {
-                check[middle] = true;
-                ans[middle] = ans[current] + 1;
-                q.add(middle);
-            }
-            if (right >= 0 && right <= 100000 && !check[right]) {
-                check[right] = true;
-                ans[right] = ans[current] + 1;
-                q.add(right);
-            }
+		while (!queue.isEmpty()) {
+			Position pos = queue.poll();
 
-            if (right == k || middle == k || left == k) {
-                break;
-            }
-        }
-    }
+			int currentPos = pos.pos;
+			int currentStep = pos.step;
+
+			if(currentPos == m) {
+				answer = currentStep;
+				break;
+			}
+
+			int nextPos1 = currentPos - 1;
+			int nextPos2 = currentPos + 1;
+			int nextPos3 = currentPos * 2;
+
+			if (isValid(nextPos1) && !check[nextPos1]) {
+				queue.add(new Position(nextPos1, currentStep + 1));
+				check[nextPos1] = true;
+				step[nextPos1] = Math.max(step[nextPos1], currentStep + 1);
+			}
+
+			if (isValid(nextPos2) && !check[nextPos2]) {
+				queue.add(new Position(nextPos2, currentStep + 1));
+				check[nextPos2] = true;
+				step[nextPos2] = Math.max(step[nextPos2], currentStep + 1);
+			}
+
+			if (isValid(nextPos3) && !check[nextPos3]) {
+				queue.add(new Position(nextPos3, currentStep + 1));
+				check[nextPos3] = true;
+				step[nextPos3] = Math.max(step[nextPos3], currentStep + 1);
+			}
+		}
+		return answer;
+	}
+
+	private static boolean isValid(int pos) {
+		return pos >= 0  && pos <= 100_000;
+	}
+
+	static class Position {
+		int pos;
+		int step;
+
+		public Position(int pos, int step) {
+			this.pos = pos;
+			this.step = step;
+		}
+	}
 }
