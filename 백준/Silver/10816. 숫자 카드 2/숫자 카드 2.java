7bc23aee22c81b0f886a4099f2f1be9e9eac34ce
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2025-12-15 21:02:43 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1076 ms, Memory: 172184 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,70 +1,36 @@
-import static java.util.Collections.sort;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static int n, m;
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static Map<Integer, Integer> dict = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        List<Integer> cards = Arrays.stream(br.readLine().split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        for (int i = 0; i < n; i++) {
+            int card = Integer.parseInt(st.nextToken());
+            dict.put(card, dict.getOrDefault(card, 0) + 1);
+        }
 
-        int m = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        List<Integer> answer = Arrays.stream(br.readLine().split(" "))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-        sort(cards);
-
-        for (int i = 0; i < answer.size(); i++) {
-            Integer card = answer.get(i);
-            sb.append(upperBinarySearch(cards, card) - lowerBinarySearch(cards, card)).append(" ");
+        for (int i = 0; i < m; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            sb.append(dict.getOrDefault(number, 0)).append(" ");
         }
 
         System.out.print(sb);
-    }
-
-    private static int upperBinarySearch(List<Integer> cards, Integer card) {
-        int start = 0;
-        int end = cards.size();
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            if (cards.get(mid) <= card) {
-                start = mid + 1;
-                continue;
-            }
-
-            end = mid;
-        }
-        return end;
-    }
-
-    private static int lowerBinarySearch(List<Integer> cards, Integer card) {
-        int start = 0;
-        int end = cards.size();
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-
-            if (cards.get(mid) >= card) {
-                end = mid;
-                continue;
-            }
-
-            start = mid + 1;
-        }
-        return end;
     }
 }
