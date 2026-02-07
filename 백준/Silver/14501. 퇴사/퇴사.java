d4ca5f02ead46416e41268a8500ea8314e7a1312
김동웅 <86222503+Dongwoongkim@users.noreply.github.com>
2026-02-07 15:58:56 +0900

    [Silver III] Title: 퇴사, Time: 92 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,26 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] t = new int[n + 1];
-        int[] p = new int[n + 1];
-        int[] dp = new int[n + 1];
+    static int n;
+    static int[] t = new int[16];
+    static int[] p = new int[16];
+    static int[] dp;
+    static int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            t[i] = sc.nextInt();
-            p[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n + 2];
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n; i++) {
-            if (i + t[i] <= n) {
-                // 상담을 하는경우 vs 상담을 하지 않는 경우
-                dp[i + t[i]] = Math.max(p[i] + dp[i], dp[i + t[i]]);
+        for (int i = n; i >= 1; i--) {
+            // 상담 가능한 경우
+            if (i + t[i] <= n + 1) {
+                // 상담 안하기 vs 상담하기
+                dp[i] = Math.max(dp[i + 1], p[i] + dp[i + t[i]]);
+                
+                ans = Math.max(ans, dp[i]);
+                continue;
             }
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+
+            // 상담 불가능한 경우 -> 다음날 거 가져오기
+            dp[i] = dp[i + 1];
         }
-        System.out.println(dp[n]);
+
+        System.out.println(ans);
     }
 }
